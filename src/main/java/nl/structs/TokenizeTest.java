af4a728a5703f4d15d9edaefa94bdd6dbd41d0d7
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2026-05-06 11:54:59 +0200

    move inner class to method, simplify calls

--- src/main/java/nl/structs/TokenizeTest.java
+++ src/main/java/nl/structs/TokenizeTest.java
@@ -1,47 +1,52 @@
 package nl.structs;
 
-import java.io.*;
+import java.io.PrintWriter;
+import java.util.*;
 
+import nl.structs.AnnotateFilter.*;
 import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.standard.*;
 import org.apache.lucene.analysis.tokenattributes.*;
-import org.apache.lucene.tests.analysis.*;
+import org.apache.lucene.tests.analysis.TokenStreamToDot;
 
 public class TokenizeTest {
-    TokenizeTest() {
-        try (var analyzer = new org.apache.lucene.analysis.standard.StandardAnalyzer()) {
+
+    public static void main(String[] args) {
+        tokenizeTest();
+    }
+
+    private static void tokenizeTest() {
+        try (var analyzer = new StandardAnalyzer()) {
 
             var text = "This is a test of the tokenization process";
 
             var tokenStream = analyzer.tokenStream("field", text);
 
-            var annotations = new java.util.LinkedList<nl.structs.AnnotateFilter.Annotation>();
+            var annotations = new LinkedList<Annotation>();
 
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(5, 9, "concept1"));
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(10, 14, "concept3"));
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(10, 42, "concept2"));
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(18, 21, "concept32"));
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(18, 34, "concept5"));
-            annotations.add(new nl.structs.AnnotateFilter.Annotation(35, 42, "concept8"));
+            annotations.add(new Annotation(5, 9, "concept1"));
+            annotations.add(new Annotation(10, 14, "concept3"));
+            annotations.add(new Annotation(10, 42, "concept2"));
+            annotations.add(new Annotation(18, 21, "concept32"));
+            annotations.add(new Annotation(18, 34, "concept5"));
+            annotations.add(new Annotation(35, 42, "concept8"));
 
-            tokenStream = new nl.structs.AnnotateFilter(tokenStream, annotations);
+            tokenStream = new AnnotateFilter(tokenStream, annotations);
 
             outputDot(tokenStream);
 
-            //printTokenStream(tokenStream);
+//            printTokenStream(tokenStream);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
-        new TokenizeTest();
-    }
 
     private static void outputDot(TokenStream tokenStream) throws Exception {
-        var dotwriter = new PrintWriter("graph.dot");
-        new TokenStreamToDot(null, tokenStream, dotwriter).toDot();
-        dotwriter.close();
+        try (var dotWriter = new PrintWriter("graph.dot")) {
+            new TokenStreamToDot(null, tokenStream, dotWriter).toDot();
+        }
     }
 
     private static void printTokenStream(TokenStream tokenStream) throws Exception {
